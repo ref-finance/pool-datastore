10bd8e916351a12d7caaa5abfd2e3799c7ef4d59
Ryan Mehta <ryan.mehta@gmail.com>
2021-04-20 16:28:24 -0700

    add volume

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import app from "./lib/app.js";
+import cron from "node-cron";
 
 const PORT = process.env.PORT || 7890;
+
+cron.schedule("0 * * * *", () => {
+  storePools()
+    .then(() => storeVolume())
+    .then(() => console.log("sync"))
+    .catch(console.error);
+});
 
 app.listen(PORT, () => {
   console.log(`started on ${PORT}`);
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,20 @@
 CREATE TABLE pools (
   id BIGINT PRIMARY KEY,
-  token_id_1 TEXT,
-  token_id_2 TEXT,
+  token_1_id TEXT,
+  token_2_id TEXT,
   token_1_amount TEXT,
   token_2_amount TEXT,
   fee INT,
-  shares TEXT
+  shares TEXT,
+  last_update TIMESTAMP
+);
+
+CREATE TABLE volumes (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  pool_id BIGINT REFERENCES pools(id),
+  token_1_input TEXT,
+  token_2_input TEXT,
+  token_1_output TEXT,
+  token_2_output TEXT,
+  created_at TIMESTAMP
 );
